a006eab1a5cf31f54e4d207fb856b7b29e3438c2
mohprog20 <mohprog20@gmail.com>
2025-09-18 15:27:31 +0300

    Move mouse between fingers

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import clicker,time
 import hand_tracking_base as htb
 import cv2
-
+import position_handler as ph
 
 clicker = clicker.Clicker(cooldown=1.0)
+
+pos_handler = ph.PositionHandler()
+
 def main():
+    last_distance = 0
+    new_distance = 0
+
     ptime=0
     ctime=0
     cap=cv2.VideoCapture(0)
@@ -20,10 +26,12 @@ def main():
         fps=1/(ctime-ptime)
         ptime=ctime
         cv2.putText(img,f"FPS: {int(fps)}",(10,70),cv2.FONT_HERSHEY_SIMPLEX,2,(255,0,255),2)
-
-
         cv2.imshow("Image",img)
         cv2.waitKey(1)
+        if len(lmList)!=0 and True:
+            pos1=pos_handler.get_refernce_position(lmList[4][1:3])
+            pos2=pos_handler.get_refernce_position(lmList[8][1:3])
+            clicker.move_mouse((pos2[0]+pos1[0])/2,(pos2[1]+pos1[1])/2)
 
 if __name__ == "__main__":
     main()
